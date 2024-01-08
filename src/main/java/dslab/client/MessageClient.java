b2b920f5137a5a8d7e8cb4b8f8ffc949f78f623e
OrangeJupiter09 <flovogl@gmx.at>
2024-01-08 10:49:26 +0100

    created parts of constructor

--- src/main/java/dslab/client/MessageClient.java
+++ src/main/java/dslab/client/MessageClient.java
@@ -3,10 +3,28 @@ package dslab.client;
 import java.io.InputStream;
 import java.io.PrintStream;
 
+import at.ac.tuwien.dsg.orvell.Shell;
 import dslab.ComponentFactory;
 import dslab.util.Config;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.crypto.SecretKey;
 
 public class MessageClient implements IMessageClient, Runnable {
+
+
+    private static final Log LOG = LogFactory.getLog(MessageClient.class);
+
+    private final Shell shell;
+    private final int transferPort;
+
+    private final String transferHost;
+
+    private final String componentId;
+
+    private final Config config;
+    private SecretKey secretKey;
 
     /**
      * Creates a new client instance.
@@ -17,6 +35,14 @@ public class MessageClient implements IMessageClient, Runnable {
      * @param out the output stream to write console output to
      */
     public MessageClient(String componentId, Config config, InputStream in, PrintStream out) {
+        this.config = config;
+        this.componentId = componentId;
+        this.transferPort = config.getInt("transfer.port");
+        this.transferHost = config.getString("tranfer.host");
+        this.shell = new Shell(in, out);
+
+        shell.register(this);
+        shell.setPrompt(componentId + " < ");
 
     }
 
